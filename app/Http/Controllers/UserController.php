e3c82159dcbd840b8f6e55afbdd15da2bb1e09c3
fernandes magalhaes <fernandesmtc91@gmail.com>
2025-07-13 01:27:44 +0000

    alteracao

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,15 +7,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    // Exibe a lista de usuários ou a página de um usuário específico
     public function index()
     {
-        return [
-            'id' => 1,
-            'name' => 'Francisco'
-        ];
+        return view('users.index');
+
+        // return 
+        //         'id' => 1,
+        //        'name' => 'Francisco'
+        //     ]);
     }
-    public function show(User $id)
+
+    // Exibe um usuário específico (usando Route Model Binding)
+    public function show(User $user)
     {
-        return $id;
+        return $user;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
+
 use App\http\controllers\UserController;
+use App\Http\Controllers\users;
 use Illuminate\Support\Facades\Route;
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get(
+    '/',
+    function () {
+        return view('welcome');
+    }
+);
 // routes/web.php
 
 //Route::get('admin/usuarios',[UserController::class,'index']);
-Route::get('admin/usuarios/{id}',[UserController::class,'show']);
-    
-
-
-
-    
-
-
-
-
-
+Route::get('admin/usuarios', [UserController::class, 'index']);
